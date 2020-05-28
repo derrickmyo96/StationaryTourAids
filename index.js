e02d4daa8b8986b01936a3b1cb79bac88a95a969
Myo Oo <37631554+derrickmyo96@users.noreply.github.com>
2020-05-28 21:51:20 +0800

    Stationary Tablet Frontend

--- index.js
+++ index.js
@@ -6,9 +6,24 @@ var router = express.Router();
 router.get('/', function(req, res){
     res.sendFile(path.join(__dirname + '/view/main.html'));
 });
+router.get('/welcome-page', function(req, res){
+    res.sendFile(path.join(__dirname + '/view/selection.html'));
+});
+router.get('/ipad-registration', function(req, res){
+    res.sendFile(path.join(__dirname + '/view/registration.html'));
+});
+router.get('/mobile-registration', function(req, res){
+    res.sendFile(path.join(__dirname + '/view/qrcode.html'));
+});
 
 router.post('/welcome-page', function(req, res){
     res.sendFile(path.join(__dirname + '/view/scan.html'));
+});
+router.post('/ipad-registration', function(req, res){
+    res.sendFile(path.join(__dirname + '/view/registration.html'));
+});
+router.post('/mobile-registration', function(req, res){
+    res.sendFile(path.join(__dirname + '/view/qrcode.html'));
 });
 
 app.use(express.static(__dirname + '/view'));
