942366cbc0b2ae2bd265398987b2ae49e2949b1c
Myo Oo <37631554+derrickmyo96@users.noreply.github.com>
2020-06-05 10:40:27 +0800

    Minor updates to Krislab Employee page
    - Moved all navbar to <head>
    - Added login page

--- StationaryTablet/routes/krislabLogin.js
+++ StationaryTablet/routes/krislabLogin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/krislabLogin', function(req, res, next) {
-  res.render('krislabLoginr', { title: 'KrisLab Employee Site' });
+  res.render('krislabLogin', { title: 'KrisLab Employee Site' });
 });
 
 module.exports = router;
