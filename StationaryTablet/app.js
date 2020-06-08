f972407a83c7a29592fe5b6d09aa204ca25acf65
Myo Oo <37631554+derrickmyo96@users.noreply.github.com>
2020-06-08 14:23:49 +0800

    added nodemailer module
    Previous app.js crashes due to lack of nodemailer module

--- StationaryTablet/app.js
+++ StationaryTablet/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var nodemailer = require('nodemailer')
 
 //  Choose one of the mainRouter for main page
 var mainRouter = require('./routes/main');
