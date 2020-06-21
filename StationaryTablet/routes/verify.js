7ef214aaaf5d34399599cbbd1cab6a5a59595b76
Myo Oo <37631554+derrickmyo96@users.noreply.github.com>
2020-06-21 16:46:07 +0800

    Email verification done
    Email verification successful
    
    Co-Authored-By: Sarthak Ganoorkar <sarthakg16@users.noreply.github.com>

--- StationaryTablet/routes/verify.js
+++ StationaryTablet/routes/verify.js
@@ -3,20 +3,19 @@ var router = express.Router();
 var jwt = require('jsonwebtoken');
 
 /* GET home page. */
-router.get('/verify', function(req, res, next) {
-    console.log("token here:", req.body);
-    const{token} = req.body;
+router.get('/', function(req, res, next) {
+    console.log("token here:", req.query);
+    const{token} = req.query;
     if (token) {
         jwt.verify(token, process.env.JWT_TOKEN, function(err, decodedToken) {
             if (err) {
                 return res.status(400).json({error: 'Incorrect or Expired verification link.'});
             }
             var {name, email, contact} = decodedToken;
+            console.log("decodedToken", decodedToken);
+            res.render('main1', { title: 'Register for an iPad' });
         })
     };
-  res.render('verify', { title: 'Email verification' });
 });
-
-
 
 module.exports = router;
